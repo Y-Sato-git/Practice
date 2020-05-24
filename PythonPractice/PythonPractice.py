9de6786da34c44b23cf4b9f826caa356706a1f91
Y-Sato-git <s_wisteria_fuji@yahoo.co.jp>
2020-05-24 19:11:26 +0900

    2020/05/24(日)

--- PythonPractice/PythonPractice.py
+++ PythonPractice/PythonPractice.py
@@ -8,3 +8,17 @@ b = int(input("数字を入力してください :"))
 #繰り返し
 for i in range(b):
     print(a,end="")
+print()
+
+#2020/05/24
+#timeモジュール
+
+import time
+intTime = int(input("カウントダウンします。秒数を指定してください　：　"))
+
+while intTime >= 1:
+    print(intTime,end=",")
+    intTime -= 1
+    time.sleep(1)
+    if intTime == 0:
+        print("タイムアップ！")
