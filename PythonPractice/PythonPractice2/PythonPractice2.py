c8c5608d445b2bd002438318f22b5709cfe72d11
Y-Sato-git <s_wisteria_fuji@yahoo.co.jp>
2020-06-01 21:41:28 +0900

    2020/06/01(月)

--- PythonPractice/PythonPractice2/PythonPractice2.py
+++ PythonPractice/PythonPractice2/PythonPractice2.py
@@ -1,1 +1,17 @@
+#2020年6月分
+flg =  input("YYYYMMDDの日付 または ALL ⇒ ")
+print(flg)
 
+if flg == ("20200601" or "ALL"):
+
+#2020/06/01
+#例外処理
+    l = [1,2,3,4,5]
+    
+    try:
+        intNum = int(input("数字を入力してください　：　"))
+        print(l[intNum + 1])
+    except IndexError as ex:
+        print("リストにありません。エラー内容⇒{}".format(ex))
+    except Exception as ex:
+        print("エラー内容⇒{}".format(ex))
